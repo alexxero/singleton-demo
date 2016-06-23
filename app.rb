2332e49599aaa38dea826cc46358b91819542cda
alexxero <alexxero@mail.ru>
2016-06-23 14:40:43 +0300

    Logger update with instance self method

--- app.rb
+++ app.rb
@@ -4,6 +4,16 @@ class Logger
     @f = File.open 'log.txt', 'a'
   end
 
+  @@x = nil
+
+  def self.instance
+    if @@x == nil
+      @@x = Logger.new
+    end
+
+    return @@x
+  end
+
   def self.say
     puts 'haha'
   end
@@ -15,6 +25,7 @@ end
 
 logger = Logger.new
 logger.log 'hey'
+Logger.instance.log 'blablabla' *3
 #
 # logger = Logger.new
 # logger.say
