db78a9687b2a69cb2838c3255de99a3247451389
alexxero <alexxero@mail.ru>
2016-06-23 14:52:13 +0300

    Move Logger class to separate file

--- app.rb
+++ app.rb
@@ -1,31 +1,7 @@
-class Logger
-
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
-
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-
-    return @@x
-  end
-
-  def self.say
-    puts 'haha'
-  end
-
-  def log data
-    @f.puts data
-  end
-end
-
-logger = Logger.new
-logger.log 'hey'
-Logger.instance.log 'blablabla' *3
+require './logger'
+# logger = Logger.new
+# logger.log 'hey'
+Logger.instance.log 'blablabla' *13
 #
 # logger = Logger.new
 # logger.say
