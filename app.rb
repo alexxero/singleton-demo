294f5eac51c5c77ba8132af760de11bd487ad40d
alexxero <alexxero@mail.ru>
2016-06-23 14:30:41 +0300

    Logger updated

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 class Logger
+
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
   def self.say
     puts 'haha'
   end
+
+  def log data
+    @f.puts data
+  end
 end
 
-Logger.say
+logger = Logger.new
+logger.log 'hey'
 #
 # logger = Logger.new
 # logger.say
